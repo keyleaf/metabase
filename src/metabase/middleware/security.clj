30353f855e02fb8eb622a547ced1070118f67a21
jiangbo <jbkey@126.com>
2020-05-03 20:21:58 +0800

    跨域问题修复

--- src/metabase/middleware/security.clj
+++ src/metabase/middleware/security.clj
@@ -103,7 +103,9 @@
    content-security-policy-header
    (when-not allow-iframes?
      ;; Tell browsers not to render our site as an iframe (prevent clickjacking)
-     {"X-Frame-Options"                 "ALLOW-FROM https://ba.mamcharge.com https://ompm.mamcharge.com https://uias.mamcharge.com"})
+     ;{"X-Frame-Options"                 "SAMEORIGIN"})
+     {"X-Frame-Options"                 "ALLOW-FROM https://ba.mamcharge.com, https://ompm.mamcharge.com, https://uias.mamcharge.com"})
+     ;{"X-Frame-Options"                 ["ALLOW-FROM https://ba.mamcharge.com" "ALLOW-FROM https://ompm.mamcharge.com" "ALLOW-FROM https://uias.mamcharge.com"]})
    { ;; Tell browser to block suspected XSS attacks
     "X-XSS-Protection"                  "1; mode=block"
     ;; Prevent Flash / PDF files from including content from site.
